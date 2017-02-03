81d3243536c6c9ac7411c7a39142d4d6e2f0e9d8
Oliver Beckstein <oliver.beckstein@asu.edu>
2017-02-03 17:26:20 -0600

    added sanity checks to generate_file_list

--- rp/generate_file_list.py
+++ rp/generate_file_list.py
@@ -7,11 +7,11 @@ import os
 import glob
 import json
 
-TOPDIR = "tutorial-data"
+DATADIR = "tutorial-data"
 
 DIRECTORIES = {
-    'DIMS':  os.path.join(TOPDIR, "DIMS"),
-    'FRODA': os.path.join(TOPDIR, "FRODA"),
+    'DIMS':  os.path.join(DATADIR, "DIMS"),
+    'FRODA': os.path.join(DATADIR, "FRODA"),
 }
 
 
@@ -24,19 +24,21 @@ def make_file_list(method, topdir, start=None, stop=None, step=None):
 
     # contains hard coded paths
 
-    trj_paths = dict((method, glob.glob(
-        os.path.join(topdir, pth, "trajectories", "*.dcd")))
-                     for method, pth in DIRECTORIES.items())
+    trj_paths = glob.glob(
+        os.path.join(topdir, DIRECTORIES[method], "trajectories", "*.dcd"))
     top_paths = {
         'DIMS': os.path.join(topdir, DIRECTORIES['DIMS'], "topologies", "adk4ake.psf"),
         'FRODA': os.path.join(topdir, DIRECTORIES['FRODA'], "topologies", "1ake.pdb")
     }
 
-    trj = trj_paths[method][start:stop:step]
+    trj = trj_paths[start:stop:step]
     top = len(trj) * [top_paths[method]]
 
-    assert len(trj) > 0
-    assert len(top) > 0
+    if len(trj) == 0:
+	raise OSError("No trajectories for {0} in {1}".format(method, os.path.join(topdir, DIRECTORIES[method], "trajectories")))
+    
+    if not os.path.exists(top_paths[method]):
+	 raise OSError("No topology for {0} in {1}".format(method, top_paths[method]))
 
     return top, trj
 
@@ -53,7 +55,7 @@ if __name__ == "__main__":
                         help="write topology and trajectory file lists in JSON to "
                         "OUTFILE or stdout if no filename was supplied")
     parser.add_argument("-T", "--topdir", default=os.curdir,
-                        help="Path to the directory containing '{}'".format(TOPDIR))
+                        help="Path to the directory containing '{}'".format(DATADIR))
     parser.add_argument("-b", "--start", type=int, default=None,
                         help="index of first trajectory, default  None, i.e., 0")
     parser.add_argument("-e", "--stop", type=int, default=None,
@@ -65,8 +67,11 @@ if __name__ == "__main__":
     toppath = os.path.abspath(args.topdir)
 
     if not os.path.exists(toppath):
-        raise ValueError("TOPDIR {} not found".format(toppath))
+        raise ValueError("Path for --topdir {} not found".format(toppath))
+    if not os.path.exists(os.path.join(toppath, DATADIR)):
+        raise ValueError("Path for --topdir {0} does NOT contain {1}".format(toppath, DATADIR))
     echo("TOPPATH = {}".format(toppath))
+    echo("DATADIR = {}".format(os.path.join(toppath, DATADIR)))
 
     files = make_file_list('DIMS', toppath,
                            start=args.start, stop=args.stop, step=args.step)
